4e8518f95afc10d80fc9d313a949b3212ab5fc09
Scott Jones <itspinky95@hotmail.com>
2022-04-06 18:51:30 +0000

    main

---
+++ fonts/icomoon/icomoon/demo-files/demo.js
@@ -0,0 +1,32 @@
+if (!('boxShadow' in document.body.style)) {
+    document.body.setAttribute('class', 'noBoxShadow');
+}
+
+document.body.addEventListener("click", function(e) {
+    var target = e.target;
+    if (target.tagName === "INPUT" &&
+        target.getAttribute('class').indexOf('liga') === -1) {
+        target.select();
+    }
+});
+
+(function() {
+    var fontSize = document.getElementById('fontSize'),
+        testDrive = document.getElementById('testDrive'),
+        testText = document.getElementById('testText');
+
+    function updateTest() {
+        testDrive.innerHTML = testText.value || String.fromCharCode(160);
+        if (window.icomoonLiga) {
+            window.icomoonLiga(testDrive);
+        }
+    }
+
+    function updateSize() {
+        testDrive.style.fontSize = fontSize.value + 'px';
+    }
+    fontSize.addEventListener('change', updateSize, true);
+    testText.addEventListener('input', updateTest, true);
+    testText.addEventListener('change', updateTest, true);
+    updateSize();
+}());
